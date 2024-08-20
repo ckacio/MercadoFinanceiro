74240d9e562518ba5b2f2e0b0f26a2fa11aed685
Ckacio <ckacio@gmail.com>
2024-08-20 20:31:17 -0300

    Commit Inicial

---
+++ MercadoFinanceiro/PercentageOfVolumePOV.cs
@@ -0,0 +1,23 @@
+﻿
+namespace MercadoFinanceiro
+{
+    public class PercentageOfVolumePOV
+    {
+
+        /*
+         * A estratégia Percentage of Volume (POV) é utilizada quando o cliente quer executar 
+         * uma quantidade que acompanhe um determinado percentual do volume negociado de um papel 
+         * no mercado. Por exemplo, o cliente quer fazer 10% do volume do papel PETR4. 
+         * Ao serem negociadas 1000 ações no total do papel no mercado, é esperado que 100 ações 
+         * desse total tenham sido executadas por essa estratégia. A função abaixo com a 
+         * porcentagem e o número de ações negociadas informados, retorna o valor de ações que devem ser 
+         * negociadas pela estratégia POV para atingir o volume esperado. 
+         */
+        public static decimal FuncaoRetornaQuantidade(decimal porcentagem, int totalNegociado)
+        {
+            return (int)(totalNegociado * porcentagem) / ((1 - porcentagem));
+        }
+
+
+    }
+}
---
+++ MercadoFinanceiro/Program.cs
@@ -0,0 +1,11 @@
+﻿using MercadoFinanceiro;
+
+/*
+ * Considere:  
+ * decimal FuncaoRetornaQuantidade (decimal porcentagem, int totalNegociado) 
+ * FuncaoRetornaQuantidade (0.1, 900) = 100.
+ * Ou seja, quando a função receber um totalNegociado no mercado de 900 ações 
+ * e o percentual definido pelo cliente for 10%, a estratégia terá de executar 100 ações 
+ * para totalizar 1000 ações negociadas no mercado.
+ */
+Console.Write(PercentageOfVolumePOV.FuncaoRetornaQuantidade((decimal)0.1, 900));
